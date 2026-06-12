3245171fdad399a7e6ae26dec0b1a26f9c5b8c32
RoyAndresDev <royhuamanavila@gmail.com>
2026-06-11 20:32:43 -0500

    feat: add CI pipeline and initial unit test suite for the client application

--- client/src/app/page.test.tsx
+++ client/src/app/page.test.tsx
@@ -4,6 +4,7 @@ import Home from './page'
 // Mock de next/image para evitar advertencias/errores en el entorno de jsdom
 vi.mock('next/image', () => ({
   __esModule: true,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   default: (props: any) => {
     // eslint-disable-next-line @next/next/no-img-element
     return <img {...props} alt={props.alt} />
