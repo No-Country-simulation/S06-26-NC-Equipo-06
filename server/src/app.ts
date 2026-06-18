f386357e68a5c0ad41814cb929ede9cd51916a1e
Gonzalo Travieso <traviesogonzalo@gmail.com>
2026-06-17 21:20:44 -0300

    feat(middleware): implement rate limiting to prevent excessive requests

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,11 +3,30 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { errorHandler } from './middlewares/error.handler';
+import rateLimit from 'express-rate-limit';
 import authUsersRoutes from './modules/auth_users/auth.users.routes';
 
 const app = express();
 
-app.use(cors());
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 1000,
+    message: {
+        status: 429,
+        error: 'Demasiadas peticiones. Por favor, intenta de nuevo en 15 minutos.'
+    },
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+app.use(limiter)
+
+app.use(cors(
+    {
+        origin: process.env.FRONTEND_URL,
+        credentials: true,
+    }
+));
 app.use(express.json());
 app.use(cookieParser());
 
