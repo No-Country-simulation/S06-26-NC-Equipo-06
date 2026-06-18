9d9599c81573208a28fd60bc37755617aabb61af
RoyAndresDev <royhuamanavila@gmail.com>
2026-06-18 17:24:17 -0500

    feat: implement authentication service and role-based login navigation with placeholder pages

--- client/src/app/auth/login/page.tsx
+++ client/src/app/auth/login/page.tsx
@@ -3,8 +3,27 @@
 import { useFormik } from "formik";
 import { loginSchema, LoginFields } from "./login.schema";
 import { loginService } from "@/services/auth.service";
+import { Role } from "@/app/auth/types";
+import { useRouter } from "next/navigation";
+
+const redirectPerRole = (role: Role) => {
+    switch (role) {
+        case "ADMIN":
+            return "/admin";
+        case "COMPANY":
+            return "/tenant";
+        case "MUNICIPAL_ADMIN":
+            return "/municipal";
+        case "MUNICIPAL_EVALUATOR":
+            return "/municipal";
+        default:
+            return "/";
+    }
+}
 
 const Login = () => {
+    const router = useRouter();
+
     const formik = useFormik<LoginFields>({
         initialValues: {
             email: "",
@@ -12,10 +31,12 @@ const Login = () => {
         },
         validationSchema: loginSchema,
         onSubmit: async (values, { setStatus }) => {
+            console.log("Logueando...");
             setStatus(null); // Limpiar errores previos de envío
             try {
-                await loginService(values);
-                console.log("Inicio de sesión correcto");
+                const response = await loginService(values);
+                console.log("Inicio de sesión correcto, " + response.role);
+                router.push(redirectPerRole(response.role));
             } catch (error) {
                 if (error instanceof Error) {
                     setStatus({ error: error.message });
