9327b61b90167f2c8544d86e7f82fdd1e72961f7
RoyAndresDev <royhuamanavila@gmail.com>
2026-06-29 18:38:22 -0500

    feat: create reusable header component with navigation and authentication-based layout

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -3,17 +3,40 @@
 import useAuth from "@/hooks/useAuth";
 import Link from "next/link";
 
+
 const Header = () => {
     const { logout, isAuthenticated } = useAuth();
 
     return (
-        <header className="py-5 px-6 bg-[#F3FBFF] flex justify-between items-center shadow-[0_4px_16px_rgba(0,0,0,0.15)]">
+        <header className="py-5 px-6 bg-[#F3FBFF] flex justify-between md:justify-start items-center shadow-[0_4px_16px_rgba(0,0,0,0.15)]">
             <div className="flex gap-2 items-center">
                 <img src="/logo.svg" alt="Logo" width={24} height={24} />
-                <Link className="text-primary font-bold text-2xl" href="/">Lictia</Link>
+                <Link className="text-primary font-bold text-2xl md:text-[28px]" href="/">Lictia</Link>
             </div>
+            <nav className="hidden md:block ml-8">
+                <ul className="flex gap-6">
+                    <li>
+                        <Link href="/convocatorias">Convocatorias</Link>
+                    </li>
+                    <li>
+                        <Link href="/transparencia">Transparencia</Link>
+                    </li>
+                    <li>
+                        <Link href="/servicios">Servicios</Link>
+                    </li>
+                    <li>
+                        <Link href="/soporte">Soporte</Link>
+                    </li>
+                </ul>
+            </nav>
             {
-                !isAuthenticated && <ButtonRegister />
+                !isAuthenticated &&
+                <div className="flex gap-4 ml-auto items-center">
+                    <button className="px-4 py-2 text-primary font-semibold hidden md:block">
+                        Iniciar sesión
+                    </button>
+                    <ButtonRegister />
+                </div>
             }
         </header>
     );
