736fae068fed4d2285bdca3a9920f71c2767d9c9
RoyAndresDev <royhuamanavila@gmail.com>
2026-06-29 19:05:45 -0500

    feat: initialize landing page and layout structure with global styles and AuthProvider

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -6,27 +6,33 @@ export default function Home() {
       <Header />
       <main className="">
         {/* Section: Hero */}
-        <section className="bg-[#e9f2f3] gap-8 px-6 pt-16 pb-24 flex flex-col justify-center">
-          <span className="flex items-center gap-2 mx-auto text-label-text bg-label-background py-2 px-4 rounded-full font-semibold">
-            <FeatureIcon height={18} fill="#5c6675" /> Plataforma de Nueva Generación
-          </span>
-          <h1 className="font-bold leading-8.75 mx-auto text-[28px] max-w-78.5 text-center text-[#181C1D]">
-            El estándar digital para
-            las <span className="text-primary">contrataciones
-              públicas</span> en el Perú.
-          </h1>
-          <p className="text-text-1 text-center">
-            Automatizamos la validación de
-            requisitos, reducimos el papeleo manual y
-            eliminamos el riesgo de descalificación
-            en tus procesos de licitación.
-          </p>
-          <div className="flex gap-4 flex-col">
-            <button className="rounded-xl bg-primary text-white py-4 w-full font-bold">Registrar mi Empresa</button>
-            <button className="rounded-xl border border-primary text-primary py-4 w-full font-bold">Demo para entidades</button>
-          </div>
-          <div className="bg-grey-4 w-full aspect-square rounded-3xl mx-auto">
-            {/* Imagen del Hero */}
+        <section className="bg-[#e9f2f3]">
+          <div className="md:mx-auto md:max-w-7xl px-6 pt-16 pb-24 gap-16 flex flex-col md:flex-row justify-center md:justify-between">
+            {/* Contenedor del lado izquierdo/arriba */}
+            <div className="space-y-8">
+              <span className="flex items-center gap-2 mx-auto md:mx-0 text-label-text bg-label-background py-2 px-4 rounded-full font-semibold w-fit">
+                <FeatureIcon height={18} fill="#5c6675" /> Plataforma de Nueva Generación
+              </span>
+              <h1 className="font-bold leading-8.75 md:leading-13 mx-auto md:mx-0 text-[28px] md:text-[44px] max-w-78.5 md:max-w-xl text-center md:text-left text-[#181C1D]">
+                El estándar digital para
+                las <span className="text-primary">contrataciones
+                  públicas</span> en el Perú.
+              </h1>
+              <p className="text-text-1 text-center md:text-xl md:text-left md:max-w-xl">
+                Automatizamos la validación de
+                requisitos, reducimos el papeleo manual y
+                eliminamos el riesgo de descalificación
+                en tus procesos de licitación.
+              </p>
+              <div className="flex gap-4 flex-col md:max-w-xl md:mx-0 md:flex-row">
+                <button className="rounded-xl bg-primary text-white py-4 w-full font-bold">Registrar mi Empresa</button>
+                <button className="rounded-xl border border-primary text-primary py-4 w-full font-bold">Demo para entidades</button>
+              </div>
+            </div>
+            {/* Contenedor del lado derecho/abajo */}
+            <div className="bg-grey-4 w-full aspect-square rounded-3xl mx-auto md:mx-0 max-w-125">
+              {/* Imagen del Hero */}
+            </div>
           </div>
         </section>
         { /* Section: Transparencia y Eficiencia */}
--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -8,36 +8,38 @@ const Header = () => {
     const { logout, isAuthenticated } = useAuth();
 
     return (
-        <header className="py-5 px-6 bg-[#F3FBFF] flex justify-between md:justify-start items-center shadow-[0_4px_16px_rgba(0,0,0,0.15)]">
-            <div className="flex gap-2 items-center">
-                <img src="/logo.svg" alt="Logo" width={24} height={24} />
-                <Link className="text-primary font-bold text-2xl md:text-[28px]" href="/">Lictia</Link>
-            </div>
-            <nav className="hidden md:block ml-8">
-                <ul className="flex gap-6">
-                    <li>
-                        <Link href="/convocatorias">Convocatorias</Link>
-                    </li>
-                    <li>
-                        <Link href="/transparencia">Transparencia</Link>
-                    </li>
-                    <li>
-                        <Link href="/servicios">Servicios</Link>
-                    </li>
-                    <li>
-                        <Link href="/soporte">Soporte</Link>
-                    </li>
-                </ul>
-            </nav>
-            {
-                !isAuthenticated &&
-                <div className="flex gap-4 ml-auto items-center">
-                    <button className="px-4 py-2 text-primary font-semibold hidden md:block">
-                        Iniciar sesión
-                    </button>
-                    <ButtonRegister />
+        <header className="bg-[#F3FBFF] shadow-[0_4px_16px_rgba(0,0,0,0.15)]">
+            <div className="flex justify-between md:justify-start items-center max-w-7xl md:mx-auto py-5 px-6 ">
+                <div className="flex gap-2 items-center">
+                    <img src="/logo.svg" alt="Logo" width={24} height={24} />
+                    <Link className="text-primary font-bold text-2xl md:text-[28px]" href="/">Lictia</Link>
                 </div>
-            }
+                <nav className="hidden md:block ml-8">
+                    <ul className="flex gap-6">
+                        <li>
+                            <Link href="/convocatorias">Convocatorias</Link>
+                        </li>
+                        <li>
+                            <Link href="/transparencia">Transparencia</Link>
+                        </li>
+                        <li>
+                            <Link href="/servicios">Servicios</Link>
+                        </li>
+                        <li>
+                            <Link href="/soporte">Soporte</Link>
+                        </li>
+                    </ul>
+                </nav>
+                {
+                    !isAuthenticated &&
+                    <div className="flex gap-4 ml-auto items-center">
+                        <button className="px-4 py-2 text-primary font-semibold hidden md:block">
+                            Iniciar sesión
+                        </button>
+                        <ButtonRegister />
+                    </div>
+                }
+            </div>
         </header>
     );
 };
